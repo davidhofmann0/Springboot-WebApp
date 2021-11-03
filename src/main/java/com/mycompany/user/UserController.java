912c62dd82ab1fc92f6421afeafa04bc9318e724
davidhawes <dhofmann@tsn.at>
2021-11-03 10:31:46 +0100

    UserService + UserController eingefügt. Beginn mit users.html

---
+++ src/main/java/com/mycompany/user/UserController.java
@@ -0,0 +1,25 @@
+package com.mycompany.user;
+
+import org.dom4j.rule.Mode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class UserController {
+    @Autowired private UserService service;
+
+    @GetMapping("/users")
+    public String showUserList(Model model){
+        List<User> listUsers = service.listAll();
+        model.addAttribute("listUsers", listUsers);
+
+
+
+        return "users";
+
+    }
+}
--- src/main/java/com/mycompany/user/UserService.java
+++ src/main/java/com/mycompany/user/UserService.java
@@ -1,4 +1,16 @@
 package com.mycompany.user;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class UserService {
+    @Autowired private UserRepository repo;
+
+    public List<User> listAll(){
+        return (List<User>) repo.findAll();
+    }
 }
