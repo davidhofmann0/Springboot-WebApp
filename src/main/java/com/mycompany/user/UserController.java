ccd57f46bdfb17a81a8c6cfd31677c74614a0a5f
davidhawes <dhofmann@tsn.at>
2021-11-03 12:02:00 +0100

    Fertigstellung der user_form.html

--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -24,5 +25,12 @@ public class UserController {
     public String showNewForm(Model model){
         model.addAttribute("user", new User());
         return "user_form";
+    }
+
+    @PostMapping("/users/save")
+    public String saveUser(User user){
+        service.save(user);
+
+        return "redirect:/users";
     }
 }
--- src/main/java/com/mycompany/user/UserService.java
+++ src/main/java/com/mycompany/user/UserService.java
@@ -13,4 +13,8 @@ public class UserService {
     public List<User> listAll(){
         return (List<User>) repo.findAll();
     }
+
+    public void save(User user) {
+        repo.save(user);
+    }
 }
