cfff0a0319295df0343a8b3f2608f5888c2acac3
davidhawes <dhofmann@tsn.at>
2021-11-03 12:08:56 +0100

    Ausgabe, dass die Daten gespeichert werden

--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -28,9 +29,9 @@ public class UserController {
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user){
+    public String saveUser(User user, RedirectAttributes ra){
         service.save(user);
-
+        ra.addFlashAttribute("message", "The user has been saved successfully.");
         return "redirect:/users";
     }
 }
