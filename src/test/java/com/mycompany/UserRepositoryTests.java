7e2a876a075f2680df0da45fc75c5bfce84c5bd0
davidhawes <dhofmann@tsn.at>
2021-11-03 09:53:46 +0100

    Initial commit

---
+++ src/test/java/com/mycompany/UserRepositoryTests.java
@@ -0,0 +1,36 @@
+package com.mycompany;
+
+
+import com.mycompany.user.User;
+import com.mycompany.user.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTests {
+
+    @Autowired private UserRepository repo;
+
+    @Test
+    public void testAddNew(){
+        User user = new User();
+        user.setEmail("RickFlick@gmail.com");
+        user.setPassword("123456789");
+        user.setFirstName("Rick");
+        user.setLastName("Flick");
+
+        User savedUser = repo.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+
+
+    }
+
+}
