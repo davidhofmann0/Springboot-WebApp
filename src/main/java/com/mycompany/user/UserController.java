949364a2aed0cf38e58fd955bd93a1ebfb0366f2
davidhawes <dhofmann@tsn.at>
2021-11-10 09:12:03 +0100

    Fertig gestellt

--- src/main/java/com/mycompany/user/UserController.java
+++ src/main/java/com/mycompany/user/UserController.java
@@ -45,8 +45,23 @@ public class UserController {
             model.addAttribute("pageTitle", "Edit User (ID: " + id + ")");
             return "user_form";
         } catch (UserNotFoundException e) {
-            ra.addFlashAttribute("message", "The user has been saved successfully.");
+            ra.addFlashAttribute("message", e.getMessage());
             return "redirect:/users";
         }
     }
+
+    @GetMapping("/users/delete/{id}")
+    public String deleteUser(@PathVariable("id") Integer id,  RedirectAttributes ra){
+        try {
+            service.delete(id);
+            ra.addFlashAttribute("message", "The User with the ID " + id + " has been deleted");
+
+            return "user_form";
+        } catch (UserNotFoundException e) {
+            ra.addFlashAttribute("message", e.getMessage());
+        }
+        return "redirect:/users";
+
+    }
+
 }
